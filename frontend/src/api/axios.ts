ecfe96984a77468f4f6bddbe9776c0c8c7c64e97
Priyanshu Walia <waliapriyanshu07@gmail.com>
2025-12-02 05:20:02 +0530

    changed backend url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API  = axios.create({
-    baseURL:"https://formium.onrender.com/",
+    baseURL:"https://formium.onrender.com/api",
     withCredentials:true,
 })
 
