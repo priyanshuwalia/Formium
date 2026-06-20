d4110ed5b18a4f629ae200f65efc75af09633246
Priyanshu Walia <waliapriyanshu07@gmail.com>
2026-06-20 11:15:25 +0530

    push

--- formium/services/auth.service.ts
+++
@@ -1,7 +0,0 @@
-import API from "./axios";
-
-export const loginUser = (email: string, password: string)=>
-        API.post("/auth/login", { email, password})
-
-export const registerUser = (email: string, password: string) =>
-  API.post("/auth/register", { email, password });
